7c376bb5259c0f56ba9ef7bbd375be5dd027de4d
DegaZZZ <platon.makinen@aalto.fi>
2023-09-18 13:46:28 +0300

    New map

--- optest2.py
+++ optest2.py
@@ -9,8 +9,12 @@ import geopandas as gpd
 # Load GeoJSON data
 data = gpd.read_file(r'maptest3.geojson')
 data2 = gpd.read_file(r'finland-with-regions_.geojson')
+data3 = gpd.read_file(r'wtfmap1.geojson')
+
 
 app = dash.Dash(__name__)
+
+app.css.config.serve_locally = False
 
 app.css.append_css({
     'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'  # CSS reset
@@ -35,6 +39,7 @@ app.layout = html.Div(style={'height': '98vh', 'width': '100%', 'position': 'rel
             options=[
                 {'label': 'Maptest3', 'value': 'maptest3'},
                 {'label': 'Finland with Regions', 'value': 'finland-regions'},
+                {'label': 'WHAT THE FUCK???', 'value': 'wtfmap1'}
             ],
             value='maptest3',  # default value
             style={'width': '250px'}
@@ -57,6 +62,18 @@ def update_graph(selected_value):
                                zoom=4,
                                hover_name=data_source.get('nimi', ''),
                                hover_data=["pinta_ala", "posti_alue"])  # Adjust as needed
+    #else if add a third map
+
+    elif selected_value == 'wtfmap1':
+        data3['postinumeroalue'] = data3['postinumeroalue'].astype(int) 
+        data_source = data3
+        fig = px.choropleth_mapbox(data_source, geojson=data_source.geometry, locations=data_source.index,
+                               color=data_source.postinumeroalue, color_continuous_scale='geyser',
+                               mapbox_style="carto-positron",
+                               center={"lat": 64.5, "lon": 26},
+                               zoom=4,
+                               hover_name=data_source.get('nimi', '')) # Adjust as needed
+
     else:  # 'finland-regions'
         data_source = data2
         fig = px.choropleth_mapbox(data_source, geojson=data_source.geometry, locations=data_source.index,
@@ -65,7 +82,9 @@ def update_graph(selected_value):
                                center={"lat": 64.5, "lon": 26},
                                zoom=4,
                                hover_name=data_source.get('nimi', ''))  # Adjust as needed
+        
 
+        
     fig.update_layout(
         margin={"r":0,"t":0,"l":0,"b":0},
         showlegend=False,
